ba798119c7320f07d214ba9d8b075f4165d0f089
Aristotelis <aristotelis.liakatas@gmail.com>
2024-11-19 20:02:28 +0000

    Adds zlib and comments out mpich.

--- build-wrf.sh
+++ build-wrf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install requirements
+# # Install requirements
 # sudo apt update && sudo apt full-upgrade -y
 # sudo apt install -y build-essential csh m4 cmake gcc gfortran libjpeg-dev
 
@@ -9,6 +9,15 @@ WRF_ROOT=~/build_wrf
 WRF_LIBS=${WRF_ROOT}/libraries
 WRF_DEPS_BUILD_DIR=~/wrf_deps_builds
 
+MPICH_ROOT=${WRF_DEPS_BUILD_DIR}/mpich3
+MPICH_INSTALL_DIR=${WRF_LIBS}/mpich3
+MPICH_BIN=${MPICH_INSTALL_DIR}/bin
+
+ZLIB_ROOT=${WRF_DEPS_BUILD_DIR}/zlib
+ZLIB_INSTALL_DIR=${WRF_LIBS}/zlib
+ZLIB_BIN=${ZLIB_INSTALL_DIR}/bin
+
+# ********************************************
 echo "WRF Root is: " ${WRF_ROOT}
 echo "WRF libraries are located in: " ${WRF_LIBS}
 echo "WRF dependencies will be built here: " ${WRF_DEPS_BUILD_DIR}
@@ -41,29 +50,48 @@ create_directory() {
     fi
 }
 
-# Create directories
-create_directory ${WRF_ROOT}
-create_directory ${WRF_LIBS}
-create_directory ${WRF_DEPS_BUILD_DIR}
+# # Create directories
+# create_directory ${WRF_ROOT}
+# create_directory ${WRF_LIBS}
+# create_directory ${WRF_DEPS_BUILD_DIR}
 
-# Change dir to start building dependencies
-cd ${WRF_DEPS_BUILD_DIR}
+# # Change dir to start building dependencies
+# cd ${WRF_DEPS_BUILD_DIR}
 
-# MPICH
-MPICH_ROOT=${WRF_DEPS_BUILD_DIR}/mpich3
-MPICH_INSTALL_DIR=${WRF_LIBS}/mpich3
-MPICH_BIN=${MPICH_INSTALL_DIR}/bin
+# # MPICH
+# create_directory ${MPICH_ROOT}
+# cd ${MPICH_ROOT}
+# wget https://www.mpich.org/static/downloads/4.2.3/mpich-4.2.3.tar.gz
+# tar xfz mpich-4.2.3.tar.gz
+# mkdir mpich_build && cd mpich_build
+# ../mpich-4.2.3/configure --prefix=${MPICH_INSTALL_DIR}
+# make 
+# make install
 
-create_directory ${MPICH_ROOT}
-cd ${MPICH_ROOT}
-wget https://www.mpich.org/static/downloads/4.2.3/mpich-4.2.3.tar.gz
-tar xfz mpich-4.2.3.tar.gz
-mkdir mpich_build && cd mpich_build
-../mpich-4.2.3/configure --prefix=${MPICH_INSTALL_DIR}
-make 
-make install
+# Add MPICH to the PATH
 PATH=${MPICH_BIN}:${PATH}
 
-which mpicc
-which mpiexec
+# Review where the binaries are read from
+echo "which mpicc says: " `which mpicc`
+echo "which mpicc says: " `which mpiexec`
+
+# # Run a couple of quick tests to verify it is working
+# mkdir ${MPICH_ROOT}/mpi_testing && cd ${MPICH_ROOT}/mpi_testing
+# mpiexec -hosts 127.0.0.1 -n 1 hostname
+mpiexec -hosts 127.0.0.1 -n 2 ${MPICH_ROOT}/mpich_build/examples/cpi
+
+# Prepping for netCDF - with ZLIB
+cd ${WRF_DEPS_BUILD_DIR}
+
+create_directory ${ZLIB_ROOT}
+cd ${ZLIB_ROOT}
+wget https://www.zlib.net/zlib-1.3.1.tar.gz
+tar xfz zlib-1.3.1.tar.gz
+mkdir zlib_build && cd zlib_build
+../zlib-1.3.1/configure --prefix=${ZLIB_INSTALL_DIR}
+make install
+
+# Add ZLIB to the PATH
+PATH=${ZLIB_BIN}:${PATH}
+
 
